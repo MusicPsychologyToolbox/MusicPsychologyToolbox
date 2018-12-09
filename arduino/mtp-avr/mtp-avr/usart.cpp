9fd30effd48775501ddf900ee50cbc2858b33da2
MrMontag <alexander.fust.info@gmail.com>
2018-12-09 23:46:46 +0100

    mtp-avr: plain ATmega328P atmel studio project

---
+++ arduino/mtp-avr/mtp-avr/usart.cpp
@@ -0,0 +1,54 @@
+/*
+ * Music Psychology Toolbox (MPT)
+ * 
+ * Copyright (c) 2018 Alexander Fust
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * This program is free software: you can redistribute it and/or modify
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "usart.h"
+
+#include <avr/io.h>
+#include <stdio.h>
+
+void USART_init(unsigned int ubrr)
+{
+	/*Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+void USART_transmit(unsigned char data)
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSR0A & (1<<UDRE0)) );
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+void println(int value)
+{
+	char buffer[5];
+	sprintf(buffer, "%d", value);
+	for (int i=0; i<5; ++i)
+	{
+		if (!buffer[i]) break;
+		USART_transmit(buffer[i]);
+	}
+	USART_transmit('\n');
+}
